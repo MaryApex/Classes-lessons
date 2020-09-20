0d7115365946c61f34ab550b61c4c4acbeac37ef
MarianaDz <45251998+MarianaDz@users.noreply.github.com>
2020-09-20 09:25:02 -0400

    init

--- src/Enum_Home_Work/Months.java
+++ src/Enum_Home_Work/Months.java
@@ -1,26 +1,40 @@
 package Enum_Home_Work;
 
 public enum Months {
-    JANUARY (31),
-    FEBRUARY(28),
-    MARCH(31),
-    APRIL(30),
-    MAY(31),
-    JUNE(30),
-    JULY(31),
-    AUGUST(31),
-    SEPTEMBER(30),
-    OCTOBER(31),
-    NOVEMBER(30),
-    DECEMBER(31);
+    JANUARY (31, Seasons.WINTER),
+    FEBRUARY(28, Seasons.WINTER),
+    MARCH(31, Seasons.SPRING),
+    APRIL(30, Seasons.SPRING),
+    MAY(31, Seasons.SPRING),
+    JUNE(30, Seasons.SUMMER),
+    JULY(31, Seasons.SUMMER),
+    AUGUST(31, Seasons.SUMMER),
+    SEPTEMBER(30, Seasons.AUTUMN),
+    OCTOBER(31, Seasons.AUTUMN),
+    NOVEMBER(30, Seasons.AUTUMN),
+    DECEMBER(31, Seasons.WINTER);
 
-    private int days;
-    private String season;
+     int days;
+     Seasons season;
 
-    Months(int days;String season){
+    Months(int days;Seasons season){
         this.days = days;
         this.season = season;
+    }
+    public int getDays() {
+        return days;
+    }
 
+    public Seasons getSeason() {
+        return season;
+    }
+
+    @Override
+    public String toString() {
+        return "Month{ " + name() +
+                ", days=" + days +
+                ", season=" + season +
+                '}';
     }
 
 }
